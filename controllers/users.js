3fa9b55984a80e831f2017f4670fc9f8daccaf99
unknown <Leandro Severino@POAW10N09344.resource.com.br>
2018-12-13 19:22:06 -0200

    add persist capability

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,51 @@
+const User = require('../models/user');
+
 module.exports = {
-    index: (req, res, next) => {
-        res.status(200).json({
-            message: 'You requested index page'
+    indexCallback: (req, res, next) => {
+        /* Callback way */
+        User.find({}, (err, users) => {
+            if (err) {
+                next(err);
+            }
+            res.status(200).json(users);
         });
+    },
+    /* Promisses way */
+    index: (req, res, next) => {
+        /* Callback way */
+        User.find({})
+            .then(users => {
+                res.status(200).json(users);
+            })
+            .catch(err => {
+                next(err);
+            });
+    },
+    /* Callback way */
+    newUserCallback: (req, res, next) => {
+        // console.log('Request body content', req.body);
+        const newUser = new User(req.body);
+        //console.log('New User contents !', newUser);
+        newUser.save((err, user) => {
+            if (err) {
+                next(err);
+            }
+            console.log('new user added to collection !', user);
+            res.status(201).json(user);
+        });
+    },
+    /* Promisse way */
+    newUser: (req, res, next) => {
+        // console.log('Request body content', req.body);
+        const newUser = new User(req.body);
+        //console.log('New User contents !', newUser);
+        newUser.save()
+            .then(user => {
+                console.log('new user added to collection !', user);
+                res.status(201).json(user);
+            })
+            .catch(err => {
+                next(err);
+            });
     } 
 };
