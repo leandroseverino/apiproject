12983868a1d5e330a474eb40b050f301baa0acfb
unknown <Leandro Severino@POAW10N09344.resource.com.br>
2018-12-24 16:54:43 -0200

    add cars model and controller methods to persist and get the cars

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const router = express.Router();
+// const router = express.Router();
+
+const router = require('express-promise-router')();
+
 
 const UsersController = require('../controllers/users');
 
 router.route('/')
     .get(UsersController.index)
     .post(UsersController.newUser);
+
+router.route('/:id')
+    .get(UsersController.get)
+    .put(UsersController.put)
+    .patch(UsersController.patch);
+
+router.route('/:id/cars')
+    .get(UsersController.getUserCars)
+    .post(UsersController.addNewCar);
 
 module.exports=router;
